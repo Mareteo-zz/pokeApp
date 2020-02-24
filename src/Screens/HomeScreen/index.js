dc57048865bc907bb7f08cbd9cfa17c04a46543c
MATEO <mateo@MATEOs-iMac.local>
2020-02-24 01:29:33 -0500

    search pokemon by name added and working

--- src/Screens/HomeScreen/index.js
+++ src/Screens/HomeScreen/index.js
@@ -6,15 +6,19 @@ import {
   StyleSheet,
   TouchableOpacity,
   Image,
+  TextInput,
+  Alert,
 } from "react-native";
-import { Button } from "react-native-elements";
+import { Button, SearchBar } from "react-native-elements";
+import Pokedex from "pokedex-promise-v2";
+const P = new Pokedex();
 
 // Components
 import Loading from "../../Components/Common/Loading";
 import Pokeball from "../../Components/Pokeball";
 
 //Assets
-import Logo from '../../assets/pokemon-logo.png'
+import Logo from "../../assets/pokemon-logo.png";
 
 // API
 import { getAllPokemon, getPokemon } from "../../Api/pokemon";
@@ -28,6 +32,7 @@ const HomeScreen = props => {
   const [nextUrl, setNextUrl] = useState("");
   const [prevUrl, setPrevUrl] = useState("");
   const [isLoading, setLoading] = useState(true);
+  const [search, onChangeText] = useState("");
   const initialUrl = "https://pokeapi.co/api/v2/pokemon";
 
   useEffect(() => {
@@ -51,7 +56,7 @@ const HomeScreen = props => {
   };
 
   const getPrev = async () => {
-    if(!prevUrl) return;
+    if (!prevUrl) return;
     setLoading(true);
     let data = await getAllPokemon(prevUrl);
     await loadingPokemon(data.results);
@@ -71,16 +76,73 @@ const HomeScreen = props => {
     setPokemonData(_pokemonData);
   };
 
+  const getPokemonByName = () => {
+    setLoading(true);
+    if (search) {
+      P.getPokemonByName(search)
+        .then(res => {
+          setLoading(false);
+          props.navigation.navigate("Detail", { data: res })
+          console.log("by name", res);
+        })
+        .catch(err => {
+          setLoading(false);
+          Alert.alert('Pokemon not found!','Be sure you type the name correctly and all letters with lowecase')
+        });
+    } else {
+      setLoading(false);
+      Alert.alert('Ooops!','You should to type the Pokemon name in search field');
+    }
+  };
+
   return (
     <View style={customStyles.safeArea}>
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <View style={{flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around', marginVertical: 15}}>
-            <Image source={Logo} style={{width: '75%', height: 100}} />
-            <Button buttonStyle={[customStyles.btn, customStyles.primaryBtn, customStyles.smallBtn]} disabled={!prevUrl} title="Prev" onPress={() => getPrev()} />
-            <Button buttonStyle={[customStyles.btn, customStyles.primaryBtn, customStyles.smallBtn]} title="Next" onPress={() => getNext()} />
+          <View style={styles.searchContainer}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Type Pokemon by name..."
+              placeholderTextColor="white"
+              onChangeText={text => onChangeText(text)}
+              value={search}
+            />
+            <Button
+              buttonStyle={[customStyles.btn, customStyles.primaryBtn]}
+              title="Search"
+              onPress={() => getPokemonByName()}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "space-around",
+              marginVertical: 15
+            }}
+          >
+            <Image source={Logo} style={{ width: "75%", height: 100 }} />
+            <Button
+              buttonStyle={[
+                customStyles.btn,
+                customStyles.primaryBtn,
+                customStyles.smallBtn
+              ]}
+              disabled={!prevUrl}
+              title="Prev"
+              onPress={() => getPrev()}
+            />
+            <Button
+              buttonStyle={[
+                customStyles.btn,
+                customStyles.primaryBtn,
+                customStyles.smallBtn
+              ]}
+              title="Next"
+              onPress={() => getNext()}
+            />
           </View>
           <ScrollView>
             {pokemonData.map(pokemon => (
@@ -108,6 +170,18 @@ const styles = StyleSheet.create({
     fontSize: theme.FONT_SIZE_LARGE,
     color: theme.POKEMON_NAME,
     textAlign: "center"
+  },
+  searchContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly"
+  },
+  textInput: {
+    width: "70%",
+    borderWidth: 1,
+    padding: 12,
+    color: "white",
+    borderColor: "white"
   }
 });
 
